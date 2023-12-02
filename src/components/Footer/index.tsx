db0f33d688b43883ef5983fae157be659e07c0af
RosemaryAiza6611 Aiza <rosemary18aiza@gmail.com>
2023-12-02 03:00:36 +0000

    footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,32 +1,123 @@
-import styles from "./styles.module.css";
-import image from "./img/LogoSeminario.jpeg";
-import Image from "next/image";
-
+import styles from "./styles.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faRss } from "@fortawesome/free-solid-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { resourcesData, socialIconsData } from "../../utils/data";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footerSection}>
-        <div className={styles.footerText}>
-          <p>Sitemap</p>
-          <p>Home</p>
-          <p>Post</p>
-          <p>About</p>
+      <div className={styles.container}>
+        <div className={styles.imageContainer}>
+          <img src="/img/logo.jpeg" alt="Logo del sitio" />
         </div>
-        <div className={styles.footerText}>
-          <p>Conectate con nosotros</p>
-          <p>Facebook</p>
-          <p>Twitter</p>
-          <p>Tiktok</p>
-        </div>
-        <div className={styles.footerText2}>
-          <p>NEXT<span>js</span></p>
-        </div>
-        <div className={styles.footerText2}>
-          <p> ▲Vercel</p>
+
+        <div className={styles.sections}>
+          {resourcesData.map((column, columnIndex) => (
+            <div
+              key={column.title}
+              className={`${styles.section} ${styles.section}`}
+            >
+              <div
+                className={`${styles.separator} ${
+                  columnIndex === 0 ? styles.firstSeparator : ""
+                }`}
+              ></div>
+              <div className={styles.list_items}>
+                <h3 className={styles.title}>{column.title}</h3>
+                <ul>
+                  {column.items.map((item, index) => (
+                    <li key={index}>
+                      <img
+                        alt="elipse"
+                        src="/img/elipse.jpeg"
+                        className={styles.elipse}
+                      />
+                      <span
+                        className={`${styles.item_link} ${styles.item_linki}`}
+                      >
+                        {item === "ARTÍCULOS" && (
+                          <a
+                            href="http://revista.repositoriounipol.com/articles"
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            className={styles.link}
+                          >
+                            {item}
+                          </a>
+                        )}
+                        {item === "VOLÚMENES" && (
+                          <a
+                            href="http://revista.repositoriounipol.com/volumes"
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            className={styles.link}
+                          >
+                            {item}
+                          </a>
+                        )}
+                        {item === "UNIPOL.EDU.BO" && (
+                          <a
+                            href="http://unipol.edu.bo"
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            className={styles.link}
+                          >
+                            {item}
+                          </a>
+                        )}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-      <div className={styles.footerImage}>
-        <Image src={image} alt="imagen de footer" width={100} height={100} />
+
+      <div className={styles.socialFollow}>
+        {socialIconsData.map((socialIcon, index) => (
+          <div key={socialIcon.title} className={styles.itemContainer}>
+            {index > 0 && <div className={styles.separatorSocial}></div>}
+            <ul className={styles.list_items}>
+              <li>
+                <a
+                  href={socialIcon.link}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className={styles.link}
+                >
+                  <div className={`${styles.item} ${styles.itemSocial}`}>
+                    <span
+                      className={`${styles.item_link} ${styles.itemContact}`}
+                    >
+                      {socialIcon.title}
+                      {socialIcon.icon === faRss && (
+                        <FontAwesomeIcon
+                          icon={faRss}
+                          className={`${styles.icon} ${styles.fontAwesomeIcon}`}
+                        />
+                      )}
+                      {socialIcon.icon === faFacebook && (
+                        <FontAwesomeIcon
+                          icon={faFacebook}
+                          className={`${styles.icon} ${styles.fontAwesomeIcon}`}
+                        />
+                      )}
+                      {socialIcon.icon === faTwitter && (
+                        <FontAwesomeIcon
+                          icon={faTwitter}
+                          className={`${styles.icon} ${styles.fontAwesomeIcon}`}
+                        />
+                      )}{" "}
+                    </span>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+        ))}
       </div>
     </footer>
   );
---
+++ src/utils/data.ts
@@ -0,0 +1,17 @@
+
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faRss } from "@fortawesome/free-solid-svg-icons";
+
+export const resourcesData = [
+    { title: "RECURSOS", items: ["ARTÍCULOS", "VOLÚMENES"] },
+    { title: "PÁGINA", items: ["UNIPOL.EDU.BO"] },
+  ];
+  
+export   const socialIconsData = [
+    { title: "UBICACIÓN", icon: faRss, link: "https://tu-rss-feed.com" },
+    {
+      title: "SÍGUENOS EN",
+      icon: faFacebook,
+      link: "https://facebook.com/tu-cuenta",
+    },
+  ];
